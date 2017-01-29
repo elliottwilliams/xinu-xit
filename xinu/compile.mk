f56af43e0332a147775a5c67a47172d347ad16b2
Elliott Williams <e@elliottwillia.ms>
2017-01-29 08:00:25 -0500

    Wrap symbols in test utils, too

--- xinu/compile.mk
+++ xinu/compile.mk
@@ -5,41 +5,50 @@
 
 TESTS= # Define tests here or on the command line to run.
 
-TEST_UTILS_SRC=../test/xinu
-TEST_UTILS_BUILD=tests/utils
+UTIL_SRC=../test/xinu
+UTIL_BIN=tests/utils
 TEST_SRC=../tests
-TEST_BUILD=tests
+TEST_BIN=tests
 
 # Determine the object file paths for tests in $(TESTS).
 TEST_OBJS=$(foreach test,$(TESTS),$(test:%=tests/%.o)) 
-TEST_UTIL_OBJS = $(TEST_UTILS_BUILD)/test.o 
+TEST_UTIL_OBJS = $(UTIL_BIN)/test.o 
 
 # Add the test header includes to the include variable used everywhere.
 INCLUDE += -I$(TOPDIR)/test/include
 
 # Rules to compile test utils and the tests themselves.
-$(TEST_BUILD)/%.o: $(TEST_SRC)/%.c
-	$(CC) $(CFLAGS) -o $(TEST_BUILD)/$*.o $(TEST_SRC)/$*.c
-$(TEST_UTILS_BUILD)/%.o: $(TEST_UTILS_SRC)/%.c $(TEST_UTILS_BUILD)/tests.def
-	$(CC) $(CFLAGS) -include $(TEST_UTILS_BUILD)/tests.def -o $(TEST_UTILS_BUILD)/$*.o $(TEST_UTILS_SRC)/$*.c
+$(TEST_BIN)/%.o: $(TEST_SRC)/%.c
+	$(CC) $(CFLAGS) -o $(TEST_BIN)/$*.o $(TEST_SRC)/$*.c
+$(UTIL_BIN)/%.o: $(UTIL_SRC)/%.c $(UTIL_BIN)/tests.def
+	$(CC) $(CFLAGS) -include $(UTIL_BIN)/tests.def -o $(UTIL_BIN)/$*.o $(UTIL_SRC)/$*.c
 
 # Keep track of the tests included into this build of Xinu.
-$(TEST_UTILS_BUILD)/testsym: $(TEST_OBJS)
+$(UTIL_BIN)/testsym: $(TEST_OBJS)
 	@echo 'storing test symbols...'
-	@find $(TEST_BUILD) -name \*.o -exec nm {} -g \; | awk 'NF>=3 {print $$3}' | sort | uniq > $(TEST_UTILS_BUILD)/testsym
+	@find $(TEST_BIN) -name \*.o -exec nm {} -g \; | awk 'NF>=3 {print $$3}' \
+		| sort | uniq > $(UTIL_BIN)/testsym
 
-# Generate command line arguments for ld that wrap functions mocked in tests.
-$(TEST_UTILS_BUILD)/wrapargs: $(TEST_UTILS_BUILD)/testsym
-	@cat $(TEST_UTILS_BUILD)/testsym | sed -n 's/^__wrap_\(.*\)/--wrap=\1/p' > $(TEST_UTILS_BUILD)/wrapargs
+$(UTIL_BIN)/testutilsym: $(TEST_UTIL_OBJS)
+	@find $(UTIL_BIN) -name \*.o -exec nm {} -g \; | awk 'NF>=3 {print $$3}' \
+		| sort | uniq > $(UTIL_BIN)/testutilsym
+
+# Generate command line arguments for ld that wrap functions mocked in tests
+# and test utils.
+$(UTIL_BIN)/wrapargs: $(UTIL_BIN)/testsym $(UTIL_BIN)/testutilsym
+	@cat $(UTIL_BIN)/testsym $(UTIL_BIN)/testutilsym \
+		| sed -n 's/^__wrap_\(.*\)/--wrap=\1/p' > $(UTIL_BIN)/wrapargs
 
 # Generate a header that declares all tests in this build. It will be included
-# in all objects in TEST_UTILS_BUILD.
-$(TEST_UTILS_BUILD)/tests.def: $(TEST_UTILS_BUILD)/testsym
-	@echo 'struct test_s;' > $(TEST_UTILS_BUILD)/tests.def
-	@cat $(TEST_UTILS_BUILD)/testsym | sed -n 's/^\(test_.*\)/extern struct test_s \1;/p' >> $(TEST_UTILS_BUILD)/tests.def
-	@echo 'struct test_s * all_tests[] = {' >> $(TEST_UTILS_BUILD)/tests.def
-	@cat $(TEST_UTILS_BUILD)/testsym | sed -n 's/^\(test_.*\)/\&\1, /p' >> $(TEST_UTILS_BUILD)/tests.def
-	@echo '(struct test_s *) 0};' >> $(TEST_UTILS_BUILD)/tests.def
+# in all objects in UTIL_BIN.
+$(UTIL_BIN)/tests.def: $(UTIL_BIN)/testsym
+	@echo 'struct test_s;' > $(UTIL_BIN)/tests.def
+	@cat $(UTIL_BIN)/testsym | sed -n 's/^\(test_.*\)/extern struct test_s \1;/p' \
+		>> $(UTIL_BIN)/tests.def
+	@echo 'struct test_s * all_tests[] = {' >> $(UTIL_BIN)/tests.def
+	@cat $(UTIL_BIN)/testsym | sed -n 's/^\(test_.*\)/\&\1, /p' \
+		>> $(UTIL_BIN)/tests.def
+	@echo '(struct test_s *) 0};' >> $(UTIL_BIN)/tests.def
 
 # The main target for running tests. 
 test: $(CONFH) $(TEST_OBJS) test_xinu
@@ -50,10 +59,10 @@ test_xinu: TEST_REFS = $(TEST_FNS:%=&test_%,)
 test_xinu: TEST_DECLARATIONS = $(TEST_FNS:%=extern test_t test_%;)
 test_xinu: CFLAGS += -DTESTS_ENABLED
 test_xinu: OBJ_FILES += $(TEST_OBJS) $(TEST_UTIL_OBJS)
-test_xinu: LDFLAGS += @$(TEST_UTILS_BUILD)/wrapargs
-test_xinu: $(TEST_UTILS_BUILD)/wrapargs $(TEST_UTIL_OBJS) xinu 
+test_xinu: LDFLAGS += @$(UTIL_BIN)/wrapargs
+test_xinu: $(UTIL_BIN)/wrapargs $(TEST_UTIL_OBJS) xinu 
 
 clean_tests:
-	@rm -f $(TEST_BUILD)/*.o $(TEST_UTILS_BUILD)/{testsym,wrapargs,tests.def} $(TEST_UTILS_BUILD)/*.o
+	@rm -f $(TEST_BIN)/*.o $(UTIL_BIN)/{testsym,testutilsym,wrapargs,tests.def} $(UTIL_BIN)/*.o
 
 .PHONY: test test_xinu clean_tests
